e1846cc79ffd7a83a076e490958995218c7e8f46
ikki02 <B1200315@gl.aiu.ac.jp>
2018-09-02 22:11:51 +0900

    xgboost added

--- DataScience/Scoring.py
+++ DataScience/Scoring.py
@@ -5,6 +5,22 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score, KFold, TimeSeriesSplit, GroupKFold, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
+
+# ランダムフォレストの重要な概念
+# n_estimators: 決定木をいくつ作るか指定するパラメータ。以下のパラメータの影響を受ける。大体数百から数千。
+# bootstrap: n個のデータからサンプルを重複ありでn回選ぶ手法。組み合わせ数としては、nPnみたいな感じ。
+# max_features: 決定木を作る際、何個の特徴量（列）をサンプリングするか指定するパラメータ。
+# つまり、bootstrapとmax_featuresでn_estimators個のﾃﾞｰﾀｾｯﾄと対応する決定木を作り、アンサンブル学習する。
+# ﾗﾝﾀﾞﾑﾌｫﾚｽﾄや勾配ﾌﾞ-ｽﾃｨﾝｸﾞは、テキストデータのような高次元の疎なデータが苦手の部類。線形モデルの方が望ましい。
+
+# xgboost（勾配ブースティングマシン）の重要な概念
+# 公式URL：[https://xgboost.readthedocs.io/en/latest/parameter.html]
+# 1つ前の決定木の誤りを次の決定木が修正するようにして、決定木を順番に作っていく。
+# n_estimators: 決定木をいくつ作るか指定するパラメータ。
+# max_depth: 事前枝刈り。各決定木の深さを制御する。デフォルトは6。
+# learning_rate: これまでの決定木の過ちをどれくらい補正するかを制御する。低いほど過学習を抑制できる。
+
 
 # ログの出力名を設定
 logger = getLogger(__name__)
@@ -44,7 +60,7 @@ def forest_submit():
 	logger.debug('make_train_data end')
 
 	logger.info('Fitting start')
-	forest = RandomForestRegressor(n_estimators=50, random_state=1)
+    forest = RandomForestRegressor(n_estimators=50, random_state=1)
 	forest.fit(X, y)
 	logger.debug('Fitting end')
 	
@@ -119,7 +135,7 @@ def forest_gscv():
 
 	logger.info('GridSearchCV start')
 	param_grid = {'n_estimators':[10, 30, 50],
-				 'random_state':[1, 2, 3]}
+				 'random_state':[1, 2, 3]}  #param_gridの探索は、1変数の組み合わせから可能。
 	logger.debug('Parameter grid:\n{}'.format(param_grid))
 	grid_search = GridSearchCV(RandomForestRegressor(), param_grid, cv=5, n_jobs=4)  #n_jobsで使用するCPUの数を指定できる。-1にすれば全部使う。
 	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
@@ -135,6 +151,84 @@ def forest_gscv():
 	logger.debug('====================')
     
 
+def xgboost_gscv():
+	#インストール＠Ubuntu
+	# 参考になったURL（以下、手順）：[https://qiita.com/msrks/items/e3e958c04a5167575c41]
+    # cd <workspace>
+	# git clone --recursive https://github.com/dmlc/xgboost
+	# cd xgboost; make -j4
+	# pip show setuptoolsでsetuptoolsのチェック。パスが通っていることも重要。
+	# cd python-package; sudo python setup.py install
+
+	logger.info('xgboostRegressor start')
+
+	logger.debug('make_train_data start')
+	train = pd.read_csv('./result_tmp/scaled_train.csv')
+	y = train['item_cnt_month']
+	X = train.drop(['item_cnt_month'], axis=1).values
+	logger.debug('make_train_data end')
+
+	logger.info('GridSearchCV start')
+	reg = xgb.XGBRegressor()
+	param_grid = {'max_depth': [2, 4, 6], 'n_estimators': [50, 100, 500]}
+	logger.debug('Parameter grid:\n{}'.format(param_grid))
+	grid_search = GridSearchCV(reg, param_grid, cv=5, n_jobs=-1)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+	grid_search.fit(X_train, y_train)
+	logger.info('Best GridSearchCV parameters_xgb: {}'.format(grid_search.best_params_))
+	logger.info('Best GridSearchCV score_xgb: {}'.format(grid_search.best_score_))
+	logger.info('Test set score_xgb: {:.2f}'.format(grid_search.score(X_test, y_test)))
+	results = pd.DataFrame(grid_search.cv_results_)
+	results.to_csv('./result_tmp/GridSearch_xgb.csv', encoding='utf-8-sig', index=False)
+	logger.debug('GridSearchCV end')
+	
+	#EDAしたいときコメントアウトはずす。
+	#xgbr = xgb.XGBRegressor(**grid_search.best_params_)
+	#xgbr.fit(X_train, y_train)
+	
+	#fti = pd.Series(xgbr.feature_importances_, index=train.columns)
+	#fti = fti.sort_values()
+	#logger.debug(fti)
+	#fti.plot(kind='barh')
+	#plt.title('feature importance from xgboost')
+	#plt.show()
+	
+	logger.debug('xgboostRegressor end')
+	logger.debug('====================')
+
+    
+def xgboost_submit():
+	logger.info('xgboostRegressor start')
+	logger.debug('make_train_data start')
+	#train = pd.read_csv('./result_tmp/scaled_train.csv')
+	train = pd.read_csv('./result_tmp/scaled_train_DateBlockNum.csv')
+	#train = train[train['date_block_num']==33]  #直近1ヶ月
+	train = train.loc[(30<train['date_block_num'])&(train['date_block_num']<=33)]  #直近3m
+	
+	y = train['item_cnt_month']
+	X = train.drop(['item_cnt_month', 'date_block_num'], axis=1).values
+	#X = train.drop(['item_cnt_month'], axis=1).values
+	#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+	logger.debug('make_train_data end')
+
+	logger.info('Fitting start')
+	xgbr = xgb.XGBRegressor(max_depth=6, n_estimators=1000)
+	xgbr.fit(X, y)
+	logger.debug('Fitting end')
+
+	logger.info('Scoring start')
+	#logger.info('Accuracy on test set: {:.3f}'.format(.score(X_test, y_test)))
+	test_data = load_test_data()
+	test = test_data.drop(['ID'], axis=1).values
+	
+	submission = load_submission()
+	submission['item_cnt_month'] = xgbr.predict(test).astype(np.float16).clip(0., 20.)
+	submission.to_csv('./result_tmp/submit_180902_31-33_xgb.csv', encoding='utf-8-sig', index=False)
+	logger.info('submission:\n{}'.format(submission.head()))
+	logger.debug('xgboostRegressor end')
+	logger.debug('====================')
+    
+    
 if __name__ == '__main__':
 	forest_submit()
 	#forest_cv()
